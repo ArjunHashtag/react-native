e7ba594e78a77e01a6f110c1d654b681e6c6257c
ArjunHashtag <arjun.sasi@hashtag-ca.com>
2021-12-12 17:08:44 +0530

    some changes

--- screens/Home.js
+++ screens/Home.js
@@ -1,146 +1,148 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, ScrollView } from "react-native";
 import t from "../theme";
 
 const Home = () => {
   return (
-    <View style={[t.itemsCenter, t.flex1]}>
-      <View style={[t.bgPrimary, t.wFull]}>
-        <Text style={[t.textWhite, t.fontMonoBold, t.p5]}>Dashboard</Text>
+    <ScrollView style={[t.bgPrimary]}>
+      <View style={[t.itemsCenter, t.flex1]}>
+        <View style={[t.bgPrimary, t.wFull]}>
+          <Text style={[t.textWhite, t.fontMonoBold, t.p5]}>Dashboard</Text>
+        </View>
+        <View style={[t.bgPrimary, t.wFull]}>
+          <Text
+            style={[
+              t.textWhite,
+              t.fontMonoBold,
+              t.pY5,
+              t.pX10,
+              t.bgSecondary,
+              t.mY2,
+            ]}
+          >
+            Home
+          </Text>
+        </View>
+        <View style={[t.bgPrimary, t.wFull]}>
+          <Text
+            style={[
+              t.textWhite,
+              t.fontMonoBold,
+              t.pY5,
+              t.pX10,
+              t.bgSecondary,
+              t.mY2,
+            ]}
+          >
+            Profile
+          </Text>
+        </View>
+        <View style={[t.bgPrimary, t.wFull]}>
+          <Text
+            style={[
+              t.textWhite,
+              t.fontMonoBold,
+              t.pY5,
+              t.pX10,
+              t.bgSecondary,
+              t.mY2,
+            ]}
+          >
+            Login
+          </Text>
+        </View>
+        <View style={[t.bgPrimary, t.wFull]}>
+          <Text style={[t.textWhite, t.fontMonoBold, t.p5]}>Dashboard</Text>
+        </View>
+        <View style={[t.bgPrimary, t.wFull]}>
+          <Text
+            style={[
+              t.textWhite,
+              t.fontMonoBold,
+              t.pY5,
+              t.pX10,
+              t.bgSecondary,
+              t.mY2,
+            ]}
+          >
+            Home
+          </Text>
+        </View>
+        <View style={[t.bgPrimary, t.wFull]}>
+          <Text
+            style={[
+              t.textWhite,
+              t.fontMonoBold,
+              t.pY5,
+              t.pX10,
+              t.bgSecondary,
+              t.mY2,
+            ]}
+          >
+            Profile
+          </Text>
+        </View>
+        <View style={[t.bgPrimary, t.wFull]}>
+          <Text
+            style={[
+              t.textWhite,
+              t.fontMonoBold,
+              t.pY5,
+              t.pX10,
+              t.bgSecondary,
+              t.mY2,
+            ]}
+          >
+            Login
+          </Text>
+        </View>
+        <View style={[t.bgPrimary, t.wFull]}>
+          <Text style={[t.textWhite, t.fontMonoBold, t.p5]}>Dashboard</Text>
+        </View>
+        <View style={[t.bgPrimary, t.wFull]}>
+          <Text
+            style={[
+              t.textWhite,
+              t.fontMonoBold,
+              t.pY5,
+              t.pX10,
+              t.bgSecondary,
+              t.mY2,
+            ]}
+          >
+            Home
+          </Text>
+        </View>
+        <View style={[t.bgPrimary, t.wFull]}>
+          <Text
+            style={[
+              t.textWhite,
+              t.fontMonoBold,
+              t.pY5,
+              t.pX10,
+              t.bgSecondary,
+              t.mY2,
+            ]}
+          >
+            Profile
+          </Text>
+        </View>
+        <View style={[t.bgPrimary, t.wFull]}>
+          <Text
+            style={[
+              t.textWhite,
+              t.fontMonoBold,
+              t.pY5,
+              t.pX10,
+              t.bgSecondary,
+              t.mY2,
+            ]}
+          >
+            Login
+          </Text>
+        </View>
       </View>
-      <View style={[t.bgPrimary, t.wFull]}>
-        <Text
-          style={[
-            t.textWhite,
-            t.fontMonoBold,
-            t.pY5,
-            t.pX10,
-            t.bgSecondary,
-            t.mY2,
-          ]}
-        >
-          Home
-        </Text>
-      </View>
-      <View style={[t.bgPrimary, t.wFull]}>
-        <Text
-          style={[
-            t.textWhite,
-            t.fontMonoBold,
-            t.pY5,
-            t.pX10,
-            t.bgSecondary,
-            t.mY2,
-          ]}
-        >
-          Profile
-        </Text>
-      </View>
-      <View style={[t.bgPrimary, t.wFull]}>
-        <Text
-          style={[
-            t.textWhite,
-            t.fontMonoBold,
-            t.pY5,
-            t.pX10,
-            t.bgSecondary,
-            t.mY2,
-          ]}
-        >
-          Login
-        </Text>
-      </View>
-      <View style={[t.bgPrimary, t.wFull]}>
-        <Text style={[t.textWhite, t.fontMonoBold, t.p5]}>Dashboard</Text>
-      </View>
-      <View style={[t.bgPrimary, t.wFull]}>
-        <Text
-          style={[
-            t.textWhite,
-            t.fontMonoBold,
-            t.pY5,
-            t.pX10,
-            t.bgSecondary,
-            t.mY2,
-          ]}
-        >
-          Home
-        </Text>
-      </View>
-      <View style={[t.bgPrimary, t.wFull]}>
-        <Text
-          style={[
-            t.textWhite,
-            t.fontMonoBold,
-            t.pY5,
-            t.pX10,
-            t.bgSecondary,
-            t.mY2,
-          ]}
-        >
-          Profile
-        </Text>
-      </View>
-      <View style={[t.bgPrimary, t.wFull]}>
-        <Text
-          style={[
-            t.textWhite,
-            t.fontMonoBold,
-            t.pY5,
-            t.pX10,
-            t.bgSecondary,
-            t.mY2,
-          ]}
-        >
-          Login
-        </Text>
-      </View>
-      <View style={[t.bgPrimary, t.wFull]}>
-        <Text style={[t.textWhite, t.fontMonoBold, t.p5]}>Dashboard</Text>
-      </View>
-      <View style={[t.bgPrimary, t.wFull]}>
-        <Text
-          style={[
-            t.textWhite,
-            t.fontMonoBold,
-            t.pY5,
-            t.pX10,
-            t.bgSecondary,
-            t.mY2,
-          ]}
-        >
-          Home
-        </Text>
-      </View>
-      <View style={[t.bgPrimary, t.wFull]}>
-        <Text
-          style={[
-            t.textWhite,
-            t.fontMonoBold,
-            t.pY5,
-            t.pX10,
-            t.bgSecondary,
-            t.mY2,
-          ]}
-        >
-          Profile
-        </Text>
-      </View>
-      <View style={[t.bgPrimary, t.wFull]}>
-        <Text
-          style={[
-            t.textWhite,
-            t.fontMonoBold,
-            t.pY5,
-            t.pX10,
-            t.bgSecondary,
-            t.mY2,
-          ]}
-        >
-          Login
-        </Text>
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
